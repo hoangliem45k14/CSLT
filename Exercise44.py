2d11be43267bf6af48268a71fb58f310da9e45ec
ThuyTrang45K <150211818+ThuyTrang45K@users.noreply.github.com>
2023-11-08 12:12:54 +0700

    Add files via upload

---
+++ Exercise44.py
@@ -0,0 +1,10 @@
+n=input("Nhap ngay: ")
+k=input("Nhap thang: ")
+if n=="1" and k=="1":
+    print("New year's day")
+elif n==1 and k==7:
+    print("Canada day")
+elif n==25 and k==12:
+    print("Chrismas day")
+else:
+    print("Khong tuong ung voi mot ngay le co dinh nao")
